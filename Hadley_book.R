03f2556498fd8cc3bdf64f47ed8a08ef51739646
Kristen <kristen.kieta@gmail.com>
2017-12-12 13:25:02 -0800

    Loading packages, just a test to determine if I understand staging/commit

---
+++ Hadley_book.R
@@ -0,0 +1,13 @@
+#R Script to make sure I am actually understanding how to commit/push/pull to GitHub
+#What is staging? 
+#What does commiting really do?
+
+library(tidyr)
+library(ggplot2)
+library(timeSeries)
+library(gridExtra)
+library(grid)
+library(MASS)
+library(car)
+library(faraway)
+library(moments)
